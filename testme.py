815a70f4e6d5b380db007c22f5116dff1a9b2911
David H Post <post@pobox.com>
2015-10-17 02:45:58 -0500

    Replace tabs with 4 spaces to display proper indenting on GitHub

--- testme.py
+++ testme.py
@@ -44,51 +44,51 @@ ladders = [r13, r39, r67, r72]
 snakes = [s38, s47, s68, s99]
 
 for pin in ladders + snakes + [button]:
-	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 leds = [d1, d2, d3, d4, d5, d6]
 for led in leds:
-	GPIO.setup(led, GPIO.OUT)
+    GPIO.setup(led, GPIO.OUT)
 
 GPIO.output(d2,0)
 
 #Function to play sounds
 def play_sound(x):
-	pygame.mixer.music.load(x)
-	pygame.mixer.music.play(1)
-	while pygame.mixer.get_busy() == True:
-		continue
+    pygame.mixer.music.load(x)
+    pygame.mixer.music.play(1)
+    while pygame.mixer.get_busy() == True:
+        continue
 
 
 #Function to roll dice
 def dice():
-	roll = randint(1,6)
-	print(roll)
-	
-	# turn on the number of LEDs specified by roll
-	for led in leds[:roll]:
-		GPIO.output(led, 1)
-	
-	sleep(3)
-	
-	# turn them off
-	for led in leds[:roll]:
-		GPIO.output(led, 0)
+    roll = randint(1,6)
+    print(roll)
+    
+    # turn on the number of LEDs specified by roll
+    for led in leds[:roll]:
+        GPIO.output(led, 1)
+    
+    sleep(3)
+    
+    # turn them off
+    for led in leds[:roll]:
+        GPIO.output(led, 0)
 
 
 while True:
-	for pin in ladders:
-		if GPIO.input(pin):
-			print("button pressed")
-			sleep(0.5)
-			play_sound(ladder)
-	
-	for pin in snakes:
-		if GPIO.input(pin):
-			print("button pressed")
-			sleep(0.5)
-			play_sound(snake)
-	
-	if GPIO.input(button):
-		dice()
-		sleep(1)
+    for pin in ladders:
+        if GPIO.input(pin):
+            print("button pressed")
+            sleep(0.5)
+            play_sound(ladder)
+    
+    for pin in snakes:
+        if GPIO.input(pin):
+            print("button pressed")
+            sleep(0.5)
+            play_sound(snake)
+    
+    if GPIO.input(button):
+        dice()
+        sleep(1)
